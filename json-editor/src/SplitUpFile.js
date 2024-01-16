e4f0fc5cf8ec26c2fd23ca965370b18145bd5ad0
Connor deBoer <pg24connor@vfs.com>
2024-01-16 15:10:49 -0800

    fixed errors and got nesting working 'properly'

--- json-editor/src/SplitUpFile.js
+++ json-editor/src/SplitUpFile.js
@@ -1,5 +1,6 @@
 // Coyright © Connor deBoer 2024, All Rights Reserved
-const tab = "   ";
+const tab = "---";
+const nestingModifier = 2;
 
 export default class SplitUp
 {
@@ -10,24 +11,24 @@ export default class SplitUp
 
     // this guy is the 'public' function used in this class, it calls the approperaite 
     // function based off if it's an object, array, or normal field
-    CallType = (key, value, nesting) =>
+    CallType = (key, value, nesting, parent = "") =>
     {
         let type = this.DetermineType(value);
         if (type === "object")
         {
-            this.RenderObject(value, key, nesting);
+            this.RenderObject(value, key, nesting, parent);
             nesting--;
             return;
         }
 
         if (type === "array")
         {
-            this.RenderArray(key, value, nesting);
+            this.RenderArray(key, value, nesting, parent);
             nesting--;
             return;
         }
 
-        this.RenderValue(key, value, type, nesting);
+        this.RenderValue(key, value, type, nesting, parent);
     }
 
     // this guy figures out what type something is, it's needed because the 
@@ -44,32 +45,45 @@ export default class SplitUp
         return type;
     }
 
-    RenderObject = (object, name = "", nesting = 0) =>
+    RenderObject = (object, name = "", nesting = 0, parent = "") =>
     {
         if (name !== "") console.log(`${tab.repeat(nesting)}${name}`);
+        this.updateMarkup(
+            <div key={`${name}-container`} className="input-container">
+                <h1 className="input-title" >{tab.repeat(nesting * nestingModifier)}</h1>
+                <h1 style={{right: `${nesting * 10}px`}} className="input-title">{name}</h1>
+            </div>
+            );
         nesting++;
         for (let [key, value] of Object.entries(object))
         {
-            this.CallType(key, value, nesting)
+            this.CallType(`${key}`, value, nesting, name);
         }
     }
 
     // this guy draws an array by call the split up function, he also increments nesting
-    RenderArray = (name, array, nesting) =>
+    RenderArray = (name, array, nesting, parent = "") =>
     {
         console.log(`${tab.repeat(nesting)}${name}`);
+        this.updateMarkup(
+            <div key={`${name}-container`} className="input-container">
+                <h1 className="input-title" >{tab.repeat(nesting * nestingModifier)}</h1>
+                <h1 style={{margineLeft: `${nesting * 10}px`}} className="input-title">{name}</h1>
+            </div>
+            );
         nesting++;
         for (let i = 0; i < array.length; ++i)
         {
-            this.CallType(i, array[i], nesting);
+            this.CallType(`${i}`, array[i], nesting, name);
         }
     }
 
-    RenderValue = (key, value, type, nesting) =>
+    RenderValue = (key, value, type, nesting, parent = "") =>
     {
         console.log(`${tab.repeat(nesting)}${type} ${key} = ${value}`);
         const newMarkup = (
-            <div key={`${key}-${type}`} className="input-container" >
+            <div key={`${parent}${key}-${type}-container`} className="input-container" >
+                <h1 className="input-title" >{tab.repeat(nesting * nestingModifier)}</h1>
                 <h1 className="input-title" >{key}:</h1>
                 <input className="input-field" id={`${key}-${type}`} type={type} defaultValue={value} />
             </div>
